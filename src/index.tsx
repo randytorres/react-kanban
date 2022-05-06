b8ee0d963875dba91ebbdd95e8062428ef0576be
Randy Torres <torresr14@gmail.com>
2022-05-05 20:17:05 -0700

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import AppContainer from './app/components/AppContainer'
 import reportWebVitals from './reportWebVitals'
 
@@ -12,6 +13,12 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
